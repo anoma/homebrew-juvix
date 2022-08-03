7a39664716ee84b49e24855ffcb865e459702fb7
Jonathan Cubides <jonathan.cubides@uib.no>
2022-08-03 23:42:28 +0200

    Update juvix.rb

--- Formula/juvix.rb
+++ Formula/juvix.rb
@@ -1,7 +1,7 @@
 class Juvix < Formula
     desc "The Juvix compiler"
     homepage "https://juvix.org"
-    url "https://github.com/anoma/juvix.git
+    url "https://github.com/anoma/juvix.git"
     # url "https://github.com/anoma/juvix/archive/refs/tags/v0.2.2.tar.gz"
     # sha256 "6c4cff2655c2f13d0770c40e5473b03f25cc049edc73b4f3c4458013032f70ec"
     license "GPL-3.0"
