060db07c455e89ebb1fc2052bb400c352e96f974
Jonathan Cubides <jonathan.cubides@uib.no>
2022-08-03 18:56:16 +0200

    Use make

--- Formula/juvix.rb
+++ Formula/juvix.rb
@@ -5,25 +5,20 @@ class Juvix < Formula
     sha256 "6c4cff2655c2f13d0770c40e5473b03f25cc049edc73b4f3c4458013032f70ec"
     license "GPL-3.0"
     
-    resource("stdlib") do
-       url "https://github.com/anoma/juvix-stdlib.git", branch: "main"
-    end
-    
-    head do
-      url "https://github.com/anoma/juvix.git", branch: "main"
-    end
+    # head do
+    #   url "https://github.com/anoma/juvix.git", branch: "main"
+    # end
   
     option "with-emacs", "Install Emacs Plus v28"
     option "without-stack", "Do not install Haskell-Stack"
     
     depends_on "make" => :build
-    depends_on "stack" => [:build, :recommended]
+    depends_on "stack" => [:build]
     depends_on "d12frosted/emacs-plus/emacs-plus@28" => :optional
   
-    resource("stdlib").stage { bin.install "juvix-stdlib" }
     
     def install
-      system "make", "install"
+      system "make"
       prefix.install "README.org"
       prefix.install "LICENSE" 
     end
