ebb24fcd89bce75efc3c0ed61ca46c8b5e20927e
Jan Mas Rovira <janmasrovira@gmail.com>
2025-03-26 09:44:30 +0100

    update to v0.6.10

--- Formula/juvix.rb
+++ Formula/juvix.rb
@@ -1,16 +1,16 @@
 class Juvix < Formula
   desc "The Juvix Compiler"
   homepage "https://github.com/anoma/juvix"
-  version "0.6.9"
+  version "0.6.10"
 
   on_macos do
     on_intel do
       url  "https://github.com/anoma/juvix/releases/download/v#{version}/juvix-macos-x86_64.tar.gz"
-      sha256 "050e606f24ff6aeaa098bcb5223dcb92298279388aa93839acb7c5398d24a50e"
+      sha256 "6544d3d1dd9705287ae087dd4467fe48671d7c3b2b4da876c23c3d07aedf3114"
     end
     on_arm do
       url "https://github.com/anoma/juvix/releases/download/v#{version}/juvix-macos-aarch64.tar.gz"
-      sha256 "ba9917d0af6b05cde17b4a160dd2e1476fe6ad4364ad35d0b71cbde973ddd2ae"
+      sha256 "e5385b6d4da75ac4b30507cf2dbeafdef37e269227d871b991aa7a4e63abf5a9"
     end
   end
 
